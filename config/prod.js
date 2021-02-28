305a0dabe507dc0414a325d1c35bcea3fb98c26f
lee <ljg0230@gmail.com>
2021-02-28 22:51:17 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //heroku 에서 등록해줘야함
+    mongoURI: process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
-
 mongoose
-  .connect(
-    "mongodb+srv://admin:qwer1234@boilerplate.2vlgv.mongodb.net/boilerplate?retryWrites=true&w=majority",
+  .connect(config.mongoURI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -18,5 +27,16 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello"));
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
